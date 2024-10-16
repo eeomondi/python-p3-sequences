c1f88a63d47dd4841709d543e386c206ff81e5fd
Elvis <elvis.omondi@student.moringaschool.com>
2024-10-16 16:15:58 +0300

    commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+from sys import last_value
+
+
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        return []
+    elif length == 1:
+        return [0]
+    
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < length:
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+
+    return fibonacci_sequence[:length]
